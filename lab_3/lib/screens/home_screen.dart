33781da45ea594d25b17af5162c246681c39b908
Alexandra <alexandrataranu2002@gmail.com>
2024-11-25 20:25:20 +0200

    Adaugare laborator 3

---
+++ lab_3/lib/screens/home_screen.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+import "package:lab_3/shared/header/header.dart";
+import "package:lab_3/shared/search_input.dart";
+import "package:lab_3/shared/section_most_recomended/section_most_recommended.dart";
+import "package:lab_3/shared/section_nearest/section_nearest_barbershop.dart";
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(15),
+        child: Scaffold(
+            body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Header(),
+              SearchInput(),
+              SectionNearestBarbershop(),
+              SectionMostRecommended()
+            ],
+          ),
+        )));
+  }
+}
